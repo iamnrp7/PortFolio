58b59b409788fe08358018a59fa8b16281d14f99
Nihar Prajapati <143262287+iamnrp7@users.noreply.github.com>
2024-02-11 13:37:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,24 @@ import streamlit as st
 def main():
     st.title("Nihar Prajapati's Portfolio")
     
+    # Creating columns for layout
+    col1, col2 = st.columns([1, 1])
+    
     # Displaying your photo and social media icons in a horizontal layout
-    col1, col2 = st.beta_columns([2, 1])
     with col1:
-        st.image("my_pic.jpeg", use_column_width=True) 
+        st.image("my_pic.jpeg", width=100) 
         st.write("""
         ## About Me
         Digital Sage | Electronics and Communication Engineering Student 
         """)
+        
     with col2:
-        st.write("""
-        [![Instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/iamnrp7)
-        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/nihar-prajapati/)
-        [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/iamnrp7)
-        [![Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/NiharRPrajapati)
-        """)
+        st.markdown("""
+        <a href="https://www.instagram.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram" style="margin-right: 10px;"></a>
+        <a href="https://www.linkedin.com/in/nihar-prajapati/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" style="margin-right: 10px;"></a>
+        <a href="https://github.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub" style="margin-right: 10px;"></a>
+        <a href="https://twitter.com/NiharRPrajapati" target="_blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter" style="margin-right: 10px;"></a>
+        """, unsafe_allow_html=True)
 
     st.write("""
     ## Skills
