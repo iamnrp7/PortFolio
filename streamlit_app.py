3ab5a1018442a941740b37e190acdfa1f5c697fa
Nihar Prajapati <143262287+iamnrp7@users.noreply.github.com>
2024-02-11 13:41:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ def main():
     
     # Displaying your photo and social media icons in a horizontal layout
     with col1:
-        st.image("my_pic.jpeg", width=100) 
+        st.image("my_pic.jpeg", width=200) 
         st.write("""
         ## About Me
         Digital Sage | Electronics and Communication Engineering Student 
@@ -16,12 +16,11 @@ def main():
         
     with col2:
         st.markdown("""
-        <a href="https://www.instagram.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram" style="margin-right: 10px;"></a>
-        <a href="https://www.linkedin.com/in/nihar-prajapati/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" style="margin-right: 10px;"></a>
-        <a href="https://github.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub" style="margin-right: 10px;"></a>
-        <a href="https://twitter.com/NiharRPrajapati" target="_blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter" style="margin-right: 10px;"></a>
+        <div style="margin-bottom: 10px;"><a href="https://www.instagram.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram" style="margin-right: 10px;"></a></div>
+        <div style="margin-bottom: 10px;"><a href="https://www.linkedin.com/in/nihar-prajapati/" target="_blank"><img src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" style="margin-right: 10px;"></a></div>
+        <div style="margin-bottom: 10px;"><a href="https://github.com/iamnrp7" target="_blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub" style="margin-right: 10px;"></a></div>
+        <div style="margin-bottom: 10px;"><a href="https://twitter.com/NiharRPrajapati" target="_blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter" style="margin-right: 10px;"></a></div>
         """, unsafe_allow_html=True)
-
     st.write("""
     ## Skills
     BUILDING
